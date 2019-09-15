fda348fa05f31965bc63d4fb9e362c1b1c8d1e04
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-09-15 00:05:38 -0400

    Completed adding GCP Vision support

--- controllers/convert.js
+++ controllers/convert.js
@@ -1,3 +1,4 @@
+const vision = require('@google-cloud/vision');
 /**
  * GET /
  * Convert page.
@@ -25,6 +26,23 @@ exports.postFileUpload = (req, res) => {
     if (!req.file) {
         req.flash('errors', { msg: 'You did not select a file.' });
     } else {
+        async function quickstart(image_path) {
+            // Imports the Google Cloud client librar
+
+            // Creates a client
+            const client = new vision.ImageAnnotatorClient();
+
+            // Performs label detection on the image file
+            console.log("Image path :" + image_path);
+            const [result] = await client.labelDetection(image_path);
+            console.log("Result :" + result);
+            const labels = result.labelAnnotations;
+            console.log('Labels:');
+            labels.forEach(label => console.log(label.description));
+
+            return labels
+        }
+
         var img = fs.readFileSync(req.file.path);
         var encode_image = img.toString('base64');
 
@@ -36,6 +54,10 @@ exports.postFileUpload = (req, res) => {
                 filename: req.file.filename
             });
 
+            user.notes.processed.push({
+                desc: quickstart(req.file.path)
+            });
+
             user.save((err) => {
                 req.flash('info', { msg: 'Your image has been saved' });
                 // done(err, user);
@@ -43,6 +65,7 @@ exports.postFileUpload = (req, res) => {
         })
         req.flash('success', { msg: 'File was uploaded successfully.' });
     }
-    res.redirect('/convert');
+
+    res.redirect('/notes');
     // res.send(req.file);
 };
